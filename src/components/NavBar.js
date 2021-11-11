233285fc2a6449593f3c30f9390377c77db9ccdf
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-11 10:25:06 -0500

    working on the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,26 @@
-import React from "react"
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-export default function NavBar(){
-    return <h1>NavBar Page!</h1>
+export default function NavBar() {
+  return (
+    <header>
+      <div>
+        <nav>
+            <NavLink to="/">
+                Randy
+            </NavLink>
+            <NavLink to="/post">
+                Blog Post
+            </NavLink>
+            <NavLink to="/project">
+                Project
+            </NavLink>
+            <NavLink to="/about">
+                About Me!
+            </NavLink>
+           
+        </nav>
+      </div>
+    </header>
+  );
 }
