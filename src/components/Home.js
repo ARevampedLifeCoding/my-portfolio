3925006f410645075526b7214269b0eb87246504
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-11 11:38:49 -0500

    added some syling to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ export default function Home(){
     return (
         <main>
             <img src={image} alt="computer screen and laptop" className="absolute object-cover w-full h-full" />
-            <section>
-                <h1> Randy Langston</h1>
+            <section className="realitive flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"> Randy Langston</h1>
             </section>
         </main>
     )
