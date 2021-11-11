3169f591bbebf8ea205e52775bd542de3ece34ed
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-11 10:49:36 -0500

    styling the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,8 @@ export default function NavBar() {
   return (
     <header className="bg-red-600">
       <div className="container mx-auto flex justify-between">
-        <nav>
-          <NavLink to="/" exact>
+        <nav className="flex">
+          <NavLink to="/" exact className ="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl cursive tracking widest">
             Randy
           </NavLink>
           <NavLink to="/post">Blog Post</NavLink>
