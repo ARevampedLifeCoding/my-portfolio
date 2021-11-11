71c26deb49200726c185932dc73d89a64bc50060
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-11 12:01:09 -0500

    adding to the blog post section

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
 import sanityClient from "../client";
 
 export default function Post() {
   const [postData, setPost] = useState(null);
   useEffect(() => {
-    sanityClient.fetch(`*[_type == "post]{
+    sanityClient
+      .fetch(
+        `*[_type == "post]{
         title,
         slug,
         mainImage{
@@ -14,9 +17,29 @@ export default function Post() {
             },
             alt
         }
-    }`)
-    .then((data) => setPost(data))
-    .catch(console.error);
+    }`
+      )
+      .then((data) => setPost(data))
+      .catch(console.error);
   }, []);
-  return;
+  return (
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive">Blog Post Page</h1>
+        <h2 className ="text-lg text-gray-600 flex justify -center mb-12">Welcome to my page of blog post</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <article>
+            <Link to={"/post/" + post.slug.current} key={post.slug.current} >
+              <span>
+                <img />
+                <span>
+                  <h3></h3>
+                </span>
+              </span>
+            </Link>
+          </article>
+        </div>
+      </section>
+    </main>
+  );
 }
