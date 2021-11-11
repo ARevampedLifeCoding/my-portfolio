6c13832dbd4cbd94265e0090953eb79ba15ed3b6
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-11 09:43:48 -0500

    the routes are working but nothing is displaying to the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Switch} from "react-router-dom"
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import SinglePost from "./components/SinglePost";
@@ -6,18 +7,17 @@ import Post from "./components/Post";
 import Project from "./components/Project";
 
 function App() {
-  return 
+  return (
     <BrowserRouter>
-    <Switch>
-      <Route component={Home} path ="/" exact />
-      <Route component={About} path = "/About" />
-      <Route component={SinglePost} path = "/post/:slug" />
-      <Route component={Post} path = "/post" />
-      <Route component={Project} path = "/project" />
-
-    </Switch>
+      <Routes>
+        <Route component= {Home} path="/" exact />
+        <Route component= {About} path="/About" />
+        <Route component= {SinglePost} path="/post/:slug" />
+        <Route component= {Post} path="/post" />
+        <Route component= {Project} path="/project" />
+      </Routes>
     </BrowserRouter>
-  
+  );
 }
 
 export default App;
