e53530c437048c6e8e6f8ccd69bfd57ebbcbbdc9
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-11 10:47:16 -0500

    working on the Nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,22 +3,15 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header>
-      <div>
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
         <nav>
-            <NavLink to="/" exact>
-                Randy
-            </NavLink>
-            <NavLink to="/post">
-                Blog Post
-            </NavLink>
-            <NavLink to="/project">
-                Project
-            </NavLink>
-            <NavLink to="/about">
-                About Me!
-            </NavLink>
-           
+          <NavLink to="/" exact>
+            Randy
+          </NavLink>
+          <NavLink to="/post">Blog Post</NavLink>
+          <NavLink to="/project">Project</NavLink>
+          <NavLink to="/about">About Me!</NavLink>
         </nav>
       </div>
     </header>
