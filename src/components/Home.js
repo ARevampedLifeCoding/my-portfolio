5cd4b57ac5ea8f7fc117655c30ee3cc45c7d81b8
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-11 11:29:42 -0500

    added a background img to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react"
+import image from "../img/portfolio-background.jpeg"
 
 export default function Home(){
     return (
         <main>
-            <img />
+            <img src={image} alt="computer screen and laptop" className="absolute object-cover w-full h-full" />
             <section>
                 <h1> Randy Langston</h1>
             </section>
