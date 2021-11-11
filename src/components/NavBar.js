e645311b952e464ea262201ae81d2b67a90de480
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-11 10:51:05 -0500

    added an active class to /

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,12 @@ export default function NavBar() {
     <header className="bg-red-600">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
-          <NavLink to="/" exact className ="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl cursive tracking widest">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl cursive tracking widest"
+          >
             Randy
           </NavLink>
           <NavLink to="/post">Blog Post</NavLink>
