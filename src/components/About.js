6baa633ce0c5e65f3b2b089cb6cb06f7c025ff42
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-13 18:54:18 -0500

    working on the about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import sanityClient from "../client";
 import img from "../img/portfolio-background.jpeg";
 import imageUrlBuilder from "./@sanity/image-url";
+import BlockContent from "../../studio/schemas/blockContent";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -32,10 +33,16 @@ export default function About() {
       <img src={img} alt="computer" className="absolute w-full" />
       <div className="p-10 lg:pt-48 container mx-auto relative">
         <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-          <img src={urlFor(author.Image).url()} className="rounded w-32 h-32 ld:w-64 lg:h-64 mr-8 "/>
-          <div>
-            <h1></h1>
-            <div>BLOCK CONTENT</div>
+          <img src={urlFor(author.Image).url()} className="rounded w-32 h-32 ld:w-64 lg:h-64 mr-8 " alt={author.name}/>
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="cursive text-6xl text-green-300 mb-4">I am {" "}
+            <span className="text-green-100">{author.name}</span></h1>
+            <div className="prose lg:prose-xl text-white">
+            <BlockContent
+            blocks={author.bio}
+            projectId="iwhy8jh2"
+            dataset="production"
+          /></div>
           </div>
         </section>
       </div>
