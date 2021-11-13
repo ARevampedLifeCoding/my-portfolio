1a9c99d792ed557fbf42bf3099db458806525348
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-13 17:11:42 -0500

    adding information to project page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import project from "../../studio/schemas/project";
 import sanityClient from "../client";
 
 export default function Project() {
@@ -21,24 +22,34 @@ export default function Project() {
       .catch(console.error);
   }, []);
   return (
-      <main className="bg-green-100 min-h-screen p-12">
-          <section className="container mx-auto">
-              <h1 className ="text-5xl flex justify-content cursive">My Projects</h1>
-              <h2 className ="text-lg text-gray-600 flex justify-content mb-12">Welcome to my projects page!</h2>
-              <section>
-                  <article>
-                      <h3></h3>
-                      <div>
-                          <span></span>
-                          <span></span>
-                          <span></span>
-                          <p></p>
-                          <span></span>
-                      </div>
-                  </article>
-              </section>
-
-          </section>
-      </main>
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-content cursive">My Projects</h1>
+        <h2 className="text-lg text-gray-600 flex justify-content mb-12">
+          Welcome to my projects page!
+        </h2>
+        <section className="grid grid-cols-2 gap-8">
+          <article className="relative rounded-lg shadow-xl bd-white p-16">
+            <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+              <a
+                href={project.link}
+                alt={project.title}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {project.title}
+              </a>
+            </h3>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+              <p></p>
+              <span></span>
+            </div>
+          </article>
+        </section>
+      </section>
+    </main>
   );
 }
