3b2d3ddd6cfab21dd43c9cc3d84fb116a5ef3253
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-13 18:17:14 -0500

    added to the single post

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -10,7 +10,7 @@ function urlFor(source) {
 
 export default function SinglePost() {
   const [singlePost, setSinglePost] = useState(null);
-  const { slug } = useParam();
+  const { slug } = useParams();
 
   useEffect(() => {
     sanityClient
@@ -31,24 +31,28 @@ export default function SinglePost() {
       )
       .then((data) => setSinglePost(data[0]))
       .catch(console.error);
-  },[slug]);
+  }, [slug]);
 
-  if(!singlePost) return <div>Loading...</div>
+  if (!singlePost) return <div>Loading...</div>;
   return (
-      <main>
-          <article>
-              <header>
-                  <div>
-                      <div>
-                          <h1></h1>
-                          <div><img /></div>
-                          <p></p>
-                      </div>
-                  </div>
-                  <img />
-              </header>
-              <div>BLOCK COntent</div>
-          </article>
-      </main>
+    <main className="bg-gray-2-- min-h-screen p-12">
+      <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+        <header className="relative">
+          <div className="absolute h-full w-full flex items-center justify-center p-8">
+            <div className="bg-white bg-opacity-75 rounded p-12">
+              <h1 className="cursive text-3xl lg:text-6xl mb-4">
+                {singlePost.title}
+              </h1>
+              <div className="flex justify-center text-gray-800">
+                <img src={urlFor(singlePost.authorImage).url()}/>
+                <p></p>
+              </div>
+            </div>
+          </div>
+          <img />
+        </header>
+        <div>BLOCK COntent</div>
+      </article>
+    </main>
   );
 }
