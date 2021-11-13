c6f6230d634fd82ba18b2d6065e295887e34f3a3
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-13 18:09:46 -0500

    working on the blog post

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,7 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import sanityClient from "../client";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function SinglePost() {
-  return <h1>SinglePost Page!</h1>;
+  const [singlePost, setSinglePost] = useState(null);
+  const { slug } = useParam();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == "${slug}]{
+          title,
+          _id,
+          slug,
+          mainImage{
+             asset -> {
+                 _id
+                 url
+             } ,
+             body,
+             "name": author->name,
+            "authorImage" :author->image        }
+      }`
+      )
+      .then((data) => setSinglePost(data[0]))
+      .catch(console.error);
+  },[slug]);
+
+  if(!singlePost) return <div>Loading...</div>
+  return (
+      <main>
+          <article>
+              <header>
+                  <div>
+                      <div>
+                          <h1></h1>
+                          <div><img /></div>
+                          <p></p>
+                      </div>
+                  </div>
+                  <img />
+              </header>
+              <div>BLOCK COntent</div>
+          </article>
+      </main>
+  );
 }
