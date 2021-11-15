1658c76b1430b2ee064e267a61ed397f959fec5a
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-14 21:28:54 -0500

    added the contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+
+export default function Home(){
+    return (
+        
+            
+            <div class="container p3 border">
+      <h2>How to reach me</h2>
+      <a href="tel:3522756254">Cell</a>
+      <br />
+      <a href="mailto: arevampedlifecoding@gmail.com">Email</a>
+      <br />
+      <a href="https://www.linkedin.com/in/randylangston/" target="_blank">
+        LinkedIn
+      </a>
+      <br />
+      <a href="https://github.com/ARevampedLifeCoding" target="_blank">
+        GitHub
+      </a>
+    </div>
+        
+    )
+    
+}
