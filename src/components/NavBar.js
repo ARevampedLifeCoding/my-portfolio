9d519a6f10153ca6a03f381ebb0423cd969744e7
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-14 21:01:34 -0500

    adding in my resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,6 +37,13 @@ export default function NavBar() {
           >
             About Me
           </NavLink>
+          <NavLink
+            to="/resume"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-teal-200 hover:text-green-800 "
+            activeClassName="text-red-100 bg-green-700"
+          >
+            Resume
+          </NavLink>
         </nav>
         <div className="inline-flex py-3 px-3 my-6">
           <SocialIcon
