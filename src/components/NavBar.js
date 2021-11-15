2d738281a9f39735fda4effa21b3a3081958d695
Randy Langston <arevampedlifecoding@gmail.com>
2021-11-15 12:08:42 -0500

    removed blog post from nav until I can fix it

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,13 +16,13 @@ export default function NavBar() {
           >
             Randy
           </NavLink>
-          <NavLink
+          {/* <NavLink
             to="/post"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800 "
             activeClassName="text-red-100 bg-green-700"
           >
             Blog Post
-          </NavLink>
+          </NavLink> */}
           <NavLink
             to="/project"
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800 "
